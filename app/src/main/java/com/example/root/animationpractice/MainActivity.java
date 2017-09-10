0689718389621705e3e9e6bb364dbfdd1eca4343
collinsnjau39@gmail.com <collinsnjau39@gmail.com>
2017-09-10 17:07:33 +0300

    I have added the method to make the batman image fade out when clicked

---
+++ app/src/main/java/com/example/root/animationpractice/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.root.animationpractice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void fade(View view){
+        //get the view
+        ImageView batMan=(ImageView) findViewById(R.id.imageView);
+        batMan.animate().alpha(0f).setDuration(2000a);
+    }
+}
