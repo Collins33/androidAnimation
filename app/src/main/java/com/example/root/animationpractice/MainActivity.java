68331fd2a088f83a986a194053d0239fd58b4647
collinsnjau39@gmail.com <collinsnjau39@gmail.com>
2017-09-10 19:42:45 +0300

    I have added animation to fade out

--- app/src/main/java/com/example/root/animationpractice/MainActivity.java
+++ app/src/main/java/com/example/root/animationpractice/MainActivity.java
@@ -12,12 +12,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    /*
     public void fade(View view){
         //get the view
         //ImageView batMan=(ImageView) findViewById(R.id.imageView);
         ImageView superman=(ImageView) findViewById(R.id.imageView2);
         //batMan.animate().rotation(180f).setDuration(8000);
-        superman.animate().rotation(180f).scaleX(0.5f).scaleY(0.5f).setDuration(9000);
+        superman.animate().rotation(180f).scaleX(0.5f).scaleY(0.5f).setDuration(900);
+
+    }*/
+
+    public void fadeOut(View view){
+        //get view
+        ImageView superman=(ImageView) findViewById(R.id.imageView2);
+        //animate the view
+        superman.animate().alpha(0.5f).setDuration(5000);
 
     }
 
